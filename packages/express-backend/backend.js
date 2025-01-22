856728fc740e0ae0d3be955b04153244ee595447
Lifsaa <sanogosalif2003@gmail.com>
2025-01-21 19:09:17 -0800

    added POST method

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,6 +35,13 @@ const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name);
 };
+const findUserById = (id) => {
+  return users["users_list"].find((user) => user["id"] === id);
+};
+const addUser = (user) => {
+  users["users_list"].push(user)
+  return user;
+}
 
 app.use(express.json());
 
@@ -58,3 +65,22 @@ app.listen(port,() => {
     }
     
   });
+
+app.get("/users/:id", (req,res) => {
+  const id = req.params.id
+  let result = findUserById(id);
+  if (result === undefined)
+  {
+    res.status(404).send("Resource not found.")
+  }
+  else{
+    res.send(result)
+  }
+});
+
+app.post("/users", (req,res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+
+});
