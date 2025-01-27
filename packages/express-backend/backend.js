af5973c4330b83baa8dd201a2898eb03de77947f
Lifsaa <sanogosalif2003@gmail.com>
2025-01-27 09:40:59 -0800

    added cors&postuser api

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
+app.use(cors());
+app.use(express.json());
 const users = {
   users_list: [
     {
@@ -52,7 +55,7 @@ const deleteUser = (id) => {
   return true;
 };
 
-app.use(express.json());
+
 
 app.get("/", (req,res) => {
     res.send("Hello World!");
--- packages/react-frontend/src/myApp.jsx
+++ packages/react-frontend/src/myApp.jsx
@@ -1,34 +1,60 @@
-// src/MyApp.jsx
-import React, {useState} from "react";
-import Table from "./Table";
-
-import Form from "./Form";
-
+import React, { useState, useEffect } from 'react';
+import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
-    const[characters,setCharacters] = useState([]);
-    function removeOneCharacter(index)
-{
-    const updated = characters.filter((character,i) =>
-    {
-        return i !== index;
-    });
-    setCharacters(updated);
+    const [characters, setCharacters] = useState([]);
     
-}
+    function removeOneCharacter(index) {
+        const updated = characters.filter((character, i) => i !== index);
+        setCharacters(updated);
+    }
+
     function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+      postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    function postUser(person) {
+      const promise = fetch("Http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(person)
+      });
+    
+      return promise;
+    }
 
+    useEffect(() => {
+        console.log('Fetching all the users');
+        fetch("http://localhost:8000/users")
+            .then(res => {
+                console.log('Raw response:', res);
+                return res.json();
+            })
+            .then(data => {
+                console.log('Parsed data:', data);
+                setCharacters(data.users_list || []);
+            })
+            .catch(error => console.error('Fetch error:', error));
+    }, []);
 
-  return (
-    <div className="container">
-      <Table 
-      characterData={characters}
-      removeCharacter={removeOneCharacter}/>
-      <Form handleSubmit ={updateList}/>
-    </div>
-  );
+    console.log('Current characters state:', characters);
+
+    return (
+        <div className="container">
+            <h1>React Users Table</h1>
+            <Table 
+                characterData={characters}
+                removeCharacter={removeOneCharacter}
+            />
+            <Form handleSubmit={updateList} />
+        </div>
+    );
 }
 
 export default MyApp;
